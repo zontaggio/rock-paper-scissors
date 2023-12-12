8d651a3af011dd0239f2a0c0440b14427f8c83de
Zonta <giordano.zonta@gmail.com>
2023-12-11 22:42:54 -0300

    Created the html and js files for the game.

--- script.js
+++ script.js
@@ -10,29 +10,6 @@ function getComputerChoice() {
       }
 }
 
-function getPlayerChoice() {
-    let playerChoice = prompt("Do you choose rock, paper or scissors? If you choose anything different, it will be randomly assigned!");
-    let filteredChoice = playerChoice.toLowerCase();
-    switch (filteredChoice) {
-        case "paper":
-            return("Paper");
-        case "rock":
-            return("Rock");
-        case "scissors":
-            return("Scissors");
-        default:
-            let randomOneToThree = Math.floor((Math.random() * 3) + 1);
-            switch (randomOneToThree) {
-                case 1:
-                    return("Paper");
-                case 2:
-                    return("Rock");
-                case 3:
-                    return("Scissors");
-              }
-      }
-}
-
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         return("You both chose " + playerSelection + ". Draw!");
@@ -51,8 +28,85 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    for (i=0; i<=4; i++) {
-        console.log(playRound(getPlayerChoice(), getComputerChoice()));
+function checkIfEnded(gameResults) {
+
+    if (computerCounter == 5) {
+        computerCounter = 0;
+        humanCounter = 0;
+        container.innerHTML = ''
+        playerScoreContainer.innerHTML = '0'
+        computerScoreContainer.innerHTML = '0'
+        resultContainer.textContent = gameResults.textContent + ' | Final score: PC wins!'
+    } else if (humanCounter == 5) {
+        computerCounter = 0;
+        humanCounter = 0;
+        container.innerHTML = ''
+        playerScoreContainer.innerHTML = '0'
+        computerScoreContainer.innerHTML = '0'
+        resultContainer.textContent = gameResults.textContent + ' | Final score: Player wins!'
+    }
+
+}
+
+function updateResults(resultText) {
+    let computerResult = resultText.includes("Computer wins!");
+    if (computerResult) {
+        computerCounter++;
+        computerScoreContainer.textContent = computerCounter;
+    }
+
+    let humanResult = resultText.includes("Player wins!");
+    if (humanResult) {
+        humanCounter++;
+        playerScoreContainer.textContent = humanCounter;
     }
 }
+
+function clearBeforeStart() {
+    if ((humanCounter == 0) && (computerCounter == 0)) {
+        resultContainer.textContent = '';
+    }
+}
+
+const container = document.querySelector('#container');
+const playerScoreContainer = document.querySelector('.playerScore');
+const computerScoreContainer = document.querySelector('.computerScore');
+const resultContainer = document.querySelector('.gameResult');
+
+computerCounter = 0;
+humanCounter = 0;
+
+const chooseRock = document.querySelector('#chooseRock');
+chooseRock.addEventListener('click', () => {
+    const gameResults = document.createElement('div');
+    gameResults.textContent = playRound("Rock", getComputerChoice());
+    clearBeforeStart();
+    updateResults(gameResults.textContent);
+    container.appendChild(gameResults);
+    checkIfEnded(gameResults);
+
+});
+
+const choosePaper = document.querySelector('#choosePaper');
+choosePaper.addEventListener('click', () => {
+    const gameResults = document.createElement('div');
+    gameResults.textContent = playRound("Paper", getComputerChoice());
+    clearBeforeStart();
+    updateResults(gameResults.textContent);
+    container.appendChild(gameResults);
+    checkIfEnded(gameResults);
+    
+});
+
+const chooseScissors = document.querySelector('#chooseScissors');
+chooseScissors.addEventListener('click', () => {
+    const gameResults = document.createElement('div');
+    gameResults.textContent = playRound("Scissors", getComputerChoice());
+    clearBeforeStart();
+    updateResults(gameResults.textContent);
+    container.appendChild(gameResults);
+    checkIfEnded(gameResults);
+    
+});
+
+
